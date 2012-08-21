aac5804085b486d71001c6f409f4adf6a5de9429
Luis Alfonso Aguilar-Sánchez <laltfuns@gmail.com>
2012-08-21 00:02:34 -0600

    Agregando mantenimiento de grupos

--- Tarea1.Rutinas/src/domain/EliminarProfesor.java
+++ Tarea1.Rutinas/src/domain/EliminarProfesor.java
@@ -10,15 +10,14 @@ import display.FrontCommand;
 
 public class EliminarProfesor extends FrontCommand {
 
-	  public void process()
-	    throws ServletException, IOException {
-	      ProfesorFinder profs =
-	        (ProfesorFinder) context.getBean("profesorFinder");
-	      String id = request.getParameter("id");
-	    ProfesorRowGateway prof = profs.find(id);
-	    if (prof!=null) {
-	      prof.delete();
-	    }
-	    response.sendRedirect("universidad/domain.ListaProfesores");
-	  }
+	public void process() throws ServletException, IOException {
+		ProfesorFinder profs = (ProfesorFinder) context
+				.getBean("profesorFinder");
+		String id = request.getParameter("id");
+		ProfesorRowGateway prof = profs.find(id);
+		if (prof != null) {
+			prof.delete();
+		}
+		response.sendRedirect("universidad/domain.ListaProfesores");
 	}
+}
